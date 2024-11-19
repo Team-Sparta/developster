070331217dd0eae83e6e2bc0878cc341712e3aa3
parkmookeun <sohappynow12@naver.com>
2024-11-19 21:09:43 +0900

    docs: sql edit

--- mookeun.sql
+++ mookeun.sql
@@ -1,24 +1,23 @@
-CREATE TABLE COMMENTS (
-                          ID BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          POST_ID BIGINT NOT NULL,
-                          PARENT_ID BIGINT,
-                          CONTENTS VARCHAR(200) NOT NULL,
-                          CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          MODIFIED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                          FOREIGN KEY (POST_ID) REFERENCES POSTS(ID) ON DELETE CASCADE,
-                          FOREIGN KEY (PARENT_ID) REFERENCES COMMENTS(ID) ON DELETE CASCADE
+CREATE TABLE comments (
+                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          post_id BIGINT NOT NULL,
+                          parent_id BIGINT,
+                          contents VARCHAR(200) NOT NULL,
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                          FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+                          FOREIGN KEY (parent_id) REFERENCES comments(id) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE COMMENT_LIKES (
-                               ID BIGINT AUTO_INCREMENT PRIMARY KEY,
-                               USER_ID BIGINT NOT NULL,
-                               POST_ID BIGINT NOT NULL,
-                               CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                               FOREIGN KEY (USER_ID) REFERENCES USERS(USER_ID) ON DELETE CASCADE,
-                               FOREIGN KEY (POST_ID) REFERENCES POSTS(ID) ON DELETE CASCADE
+CREATE TABLE comment_likes (
+                               id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                               user_id BIGINT NOT NULL,
+                               post_id BIGINT NOT NULL,
+                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                               FOREIGN KEY (user_id) REFERENCES USERS(id) ON DELETE CASCADE,
+                               FOREIGN KEY (post_id) REFERENCES POSTS(id) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE COMMENT_LIKES
     ADD CONSTRAINT UNIQUE_USER_POST
-        UNIQUE (USER_ID, POST_ID);
-
+        UNIQUE (user_id, post_id);
