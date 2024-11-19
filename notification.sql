813db0d9b410a69f4a5e75978ef90fbdb38bb166
jut <jangutm600.gmail.com>
2024-11-19 20:57:53 +0900

    알람 테이블 생성

--- notification.sql
+++ notification.sql
@@ -2,7 +2,7 @@ CREATE TABLE notifications (
     id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     message VARCHAR(255) NOT NULL,
     is_read BOOLEAN NOT NULL,
-    user_id BIGINT,
-    type ENUM('COMMENT', 'LIKE', 'FOLLOW', 'MENTION') NOT NULL
+    user_id BIGINT NOT NULL,
+    alert_type ENUM('COMMENT', 'LIKE', 'FOLLOW', 'MENTION') NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
