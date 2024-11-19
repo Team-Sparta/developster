75d1ef7adc961ed038f1dfeea2f46bc81b6f0d26
parkmookeun <sohappynow12@naver.com>
2024-11-19 20:37:10 +0900

    ìdocs: comment ëfinal

--- mookeun.sql
+++ mookeun.sql
@@ -1,11 +1,11 @@
 CREATE TABLE comments(
     id int auto_increment primary key,
     post_id int not null,
-    parent_id int not null,
+    parent_id int,
     contents varchar(200),
     created_at timestamp default current_timestamp,
     modified_at timestamp default current_timestamp on update current_timestamp,
-    foreign key (post_id) references users(user_id) on delete  cascade,
+    foreign key (post_id) references posts(id) on delete  cascade,
     foreign key (parent_id) references comments(id) on delete cascade
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
