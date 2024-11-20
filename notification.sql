3a20ead2eb5c34e42aef40ad6ca412545b8e832f
jut <jangutm600.gmail.com>
2024-11-20 10:27:39 +0900

    related 컬럼추가

--- notification.sql
+++ notification.sql
@@ -4,5 +4,6 @@ CREATE TABLE notifications (
     is_read BOOLEAN NOT NULL,
     user_id BIGINT NOT NULL,
     alert_type ENUM('COMMENT', 'LIKE', 'FOLLOW', 'MENTION') NOT NULL,
+    related_url VARCHAR(1000) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
