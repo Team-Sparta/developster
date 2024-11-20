88609334a9e5723df7567b6421e753d9a8a91fbb
jut <jangutm600.gmail.com>
2024-11-20 10:44:09 +0900

    created_at and related_url update

--- notification.sql
+++ notification.sql
@@ -4,7 +4,7 @@ CREATE TABLE notifications (
     is_read BOOLEAN NOT NULL,
     user_id BIGINT NOT NULL,
     alert_type ENUM('COMMENT', 'LIKE', 'FOLLOW', 'MENTION') NOT NULL,
-    createdAt DATETIME NOT NULL,
+    created_at DATETIME NOT NULL,
     related_url VARCHAR(250) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
